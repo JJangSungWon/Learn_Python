528b199dd30191f30e3df6be4d7905e5512dd73b
jjangsungwon <dnjs2113@gmail.com>
2020-02-04 20:57:08 +0900

    Tab을 Space 4개로 바꾸기 - 문서 파일을 읽어서 그 문서 파일 내에 있는 탭(Tab)을 공백(Space) 4개로 바꾸어주는 스크립트

---
+++ 06_파이썬 프로그래밍, 어떻게 시작해야 할까/tabto4.py
@@ -0,0 +1,16 @@
+# python tabto4.py src dst 형식으로 프로그램이 수행됨.
+
+import sys
+
+src = sys.argv[1]
+dst = sys.argv[2]
+
+f = open(src)
+tab_content = f.read()
+f.close()
+
+space_content = tab_content.replace("\t", " "*4)
+
+f = open(dst, 'w')
+f.write(space_content)
+f.close()
